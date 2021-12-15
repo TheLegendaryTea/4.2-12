47485fac8329d28147d4c02ea072718c40c3f5db
PC <PC@DESKTOP-VH0UJ4V>
2021-12-15 15:18:36 +0200

    Добавьте файлы проекта.

---
+++ 4.2 12/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _4._2_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество строк:");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов:");
+            int m = int.Parse(Console.ReadLine());
+            int[,] mas = new int[n, m];
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    mas[i, j] = rnd.Next(1, 10);
+                    Console.Write(mas[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            int count = 0;
+            foreach (int item in mas)
+            {
+                if (item == 7)
+                    count++;
+            }
+            Console.WriteLine("колличесто элементов равных цифре 7= " + count);
+        }
+    }
+}
